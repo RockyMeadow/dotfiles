c70a2321260a0c608dc89d53d93e10bbc1c734a0
Dung Trung PHAM <dtpham@linagora.com>
2021-04-23 17:40:03 +0700

    remove setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_DIR="$(cd "$(dirname  "$0")" && pwd)"
-
-ln -fsv "${BASE_DIR}/zshrc" "$HOME/.zshrc"
-ln -fsv "${BASE_DIR}/xinitrc" "$HOME/.xinitrc"
-ln -fsv "${BASE_DIR}/Xresources" "$HOME/.Xresources"
-ln -fsv "${BASE_DIR}/i3" "$HOME/.i3"
